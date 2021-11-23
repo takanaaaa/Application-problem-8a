ffc1b1a86cb2b6fff008f07648c0a3d4ba99d44c
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-11-23 14:57:57 +0000

    [Add]comment非同期通信

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,20 @@
+class BookCommentsController < ApplicationController
+  def create
+    @book = Book.find(params[:book_id])
+    book_comment = current_user.book_comments.new(book_comment_params)
+    book_comment.book_id = @book.id
+    book_comment.save
+    @book_comments = @book.book_comments
+  end
+
+  def destroy
+    BookComment.find_by(id: params[:id]).destroy
+    @book = Book.find(params[:book_id])
+    @book_comments = @book.book_comments
+  end
+
+  private
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,8 @@ class BooksController < ApplicationController
     @newbook = Book.new
     @book = Book.find(params[:id])
     @user = current_user
+    @book_comment = BookComment.new
+    @book_comments = @book.book_comments
   end
 
   def index
