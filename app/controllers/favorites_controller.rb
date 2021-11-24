80317d78049897e84e2c49a1470007a4e1a400d4
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-11-24 10:59:37 +0000

    [Add]いいね多い順・非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,12 +3,18 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 1.week).at_beginning_of_day
+    @books = Book.includes(:favorited_users).where(created_at: from...to).sort{|a,b| b.favorited_users.size <=> a.favorited_users.size}
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
+    to = Time.current.at_end_of_day
+    from = (to - 1.week).at_beginning_of_day
+    @books = Book.includes(:favorited_users).where(created_at: from...to).sort{|a,b| b.favorited_users.size <=> a.favorited_users.size}
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_many :following_user, through: "follower", source: "followed"
   has_many :follower_user, through: "followed", source: "follower"
 
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
